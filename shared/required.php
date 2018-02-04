02c853cf37706e5b964dfe7772bbbe50819b2af1
David Hiheaglo <davidhihea@gmail.com>
2018-02-04 02:43:33 +0100

    [ Update ] Remove useless line

--- shared/required.php
+++ shared/required.php
@@ -3,6 +3,5 @@
   // Required files
 
   require_once 'connexion.php';
-  require_once '../models/xhr-response.class.php';
   require_once '../models/coder.class.php';
   require_once '../models/coder-manager.class.php';
